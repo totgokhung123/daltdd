7a92892782ce5aa3508cada3a4f3b1c783fafd30
KaWuNo <nguyenthuan282003@gmail.com>
2025-01-02 15:03:06 +0700

    Lần 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-import 'package:fitness/view/main_tab/main_tab_view.dart';
 import 'package:fitness/view/on_boarding/started_view.dart';
 import 'package:flutter/material.dart' hide CarouselController;
 
@@ -19,20 +17,18 @@ class MyApp extends StatelessWidget {
       title: 'Fitness 3 in 1',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primaryColor: TColor.primaryColor1,
-        fontFamily: "Poppins"
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primaryColor: TColor.primaryColor1,
+          fontFamily: "Poppins"),
       home: const StartedView(),
     );
   }
 }
-

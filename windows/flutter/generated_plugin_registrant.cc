0250650082444caa0bc50ea805455265e8038017
totgokhung123 <chutienbinh2003@gmail.com>
2025-01-06 20:33:49 +0700

    6/1/2025_v5

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <file_selector_windows/file_selector_windows.h>
 #include <simple_animation_progress_bar/simple_animation_progress_bar_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
   SimpleAnimationProgressBarPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SimpleAnimationProgressBarPluginCApi"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  file_selector_windows
   simple_animation_progress_bar
 )
 
